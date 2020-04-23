1d16d3216c3c0dcaa12530080a3d709d25609f3f
Ananth Sridhar <nanthsree22@gmail.com>
2020-04-23 18:05:21 +0530

    Config changes and Lint fixes

--- src/server/index.js
+++ src/server/index.js
@@ -2,25 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const config = require('./resources/config');
 
 //Initializing express app
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 const PORT_NUMBER = 8080;
 
 // using separate routing for specific route section
 const routes = require('./routes');
-app.use('/api',routes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/belter',{ useNewUrlParser: true, useUnifiedTopology: true });
-const connection = mongoose.connection;
+app.use('/api', routes);
+
+mongoose.connect(`${config.MONGO_HOST}/belter`, { useNewUrlParser: true, useUnifiedTopology: true });
+const { connection } = mongoose;
 
 connection.once('open', () => {
-    console.log("Connection to MongoDB Established successfully");
-})
+    console.log('Connection to MongoDB Established successfully');
+});
 
-app.listen(PORT_NUMBER,() => {
-    console.log("Listening on "+PORT_NUMBER);
+app.listen(PORT_NUMBER, () => {
+    console.log(`Listening on ${config.PORT}`);
 });
